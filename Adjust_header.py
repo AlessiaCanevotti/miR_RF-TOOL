aff81ed384f9b62581919cbf09ea437af87b2967
AlessiaCanevotti <101797160+AlessiaCanevotti@users.noreply.github.com>
2023-11-07 11:41:43 +0100

    Update Adjust_header.py

--- Adjust_header.py
+++ Adjust_header.py
@@ -10,9 +10,14 @@ with open(user_file, 'r') as file:
 modified_lines = []
 for line in lines:
     if line.startswith('>'):
-        modified_lines.append(line.replace('\t', ' ') or line.replace('\t', '_'))
+        modified_lines.append(line.replace('\t', ' ') or line.replace('\t', '_') or line.replace(',', ' ') or line.replace(',', '_'))
     else:
         modified_lines.append(line)
 
 with open(output_file, 'w') as file:
     file.writelines(modified_lines)
+
+# Why if I run this code, it gives me the error: Traceback (most recent call last):
+  File "/home/acanevotti/miR_RF_TOOL/Adjust_header.py", line 4, in <module>
+    user_file = sys.argv[1]
+IndexError: list index out of range
