fd1a242d2c0e8366f7152ff353d00bac794e58cc
AlessiaCanevotti <101797160+AlessiaCanevotti@users.noreply.github.com>
2023-11-07 10:46:48 +0100

    Update and rename adjust_header.py to Adjust_header.py

--- Adjust_header.py
+++ Adjust_header.py
@@ -1,18 +1,16 @@
-# Read the file
-file_path = 'hseq.out'
-output_file = "hseq_out_prova.txt"
+if __name__=="__main__":
+    user_file = sys.argv[1]
+    output_file = sys.argv[2]
 
-with open(file_path, 'r') as file:
+with open(user_file, 'r') as file:
     lines = file.readlines()
 
 modified_lines = []
 for line in lines:
     if line.startswith('>'):
-        # Replace spaces with underscores in the header line
         modified_lines.append(line.replace('\t', ' ') or line.replace('\t', '_'))
     else:
         modified_lines.append(line)
 
-# Write the modified lines back to the file
 with open(output_file, 'w') as file:
     file.writelines(modified_lines)
