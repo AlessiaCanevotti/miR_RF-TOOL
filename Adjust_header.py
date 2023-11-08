21d38ac930aca82904632442fe60d289ec0003f4
AlessiaCanevotti <101797160+AlessiaCanevotti@users.noreply.github.com>
2023-11-08 18:04:39 +0100

    Update Adjust_header.py

--- Adjust_header.py
+++ Adjust_header.py
@@ -1,18 +1,1 @@
-import sys 
 
-if __name__=="__main__":
-    user_file = sys.argv[1]
-    output_file = sys.argv[2]
-
-with open(user_file, 'r') as file:
-    lines = file.readlines()
-
-modified_lines = []
-for line in lines:
-    if line.startswith('>'):
-        modified_lines.append(line.replace('\t', ' ') or line.replace('\t', '_') or line.replace(',', ' ') or line.replace(',', '_'))
-    else:
-        modified_lines.append(line)
-
-with open(output_file, 'w') as file:
-    file.writelines(modified_lines)
